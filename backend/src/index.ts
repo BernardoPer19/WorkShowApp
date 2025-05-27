dc0b4da490f0d3c820bf6e89b04d67a65a7910d7
jhoaking <wawxper08@gmail.com>
2025-05-26 15:07:57 -1100

    añadiendo login de usuarios

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { AuthController } from "./feature/Auth/controller/Auth.controller"; 
+import { AuthController } from "./feature/Auth/controller/AuthController";
 
 const app = express();
 app.use(express.json());
 
 app.post("/register", AuthController.registerUser);
-
+app.post("/login", AuthController.loginUser);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
