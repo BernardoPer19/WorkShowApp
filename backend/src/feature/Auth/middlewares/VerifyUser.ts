e1f7fac8f31d6b8fa69660be733f235ff6b5bfe9
jhoaking <wawxper08@gmail.com>
2025-05-27 05:36:30 -1100

    añadiendo middleware para verificacion de rutas segun de las cookies del usaurios

---
+++ backend/src/feature/Auth/middlewares/VerifyUser.ts
@@ -0,0 +1,24 @@
+import { UserType } from "@/types/AuthTypes";
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+export const verifyUser = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.cookies.access_token;
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "No autorizado: Token no proporcionado" });
+  }
+
+  try {
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_PASSWORD as string
+    ) as UserType;
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Token inválido o expirado" });
+  }
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { AuthController } from "./feature/Auth/controller/AuthController";
+import dotenv from "dotenv";
 
 const app = express();
 app.use(express.json());
+dotenv.config();
 
 app.post("/register", AuthController.registerUser);
 app.post("/login", AuthController.loginUser);
