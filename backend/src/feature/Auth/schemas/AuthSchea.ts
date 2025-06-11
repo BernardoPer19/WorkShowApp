ca4abf5cf5fb48e4e71831538f6afbf65eecb8a2
jhoaking <wawxper08@gmail.com>
2025-06-11 10:03:51 -1100

    refactor/ inprove code in auth

--- backend/src/feature/Auth/schemas/AuthSchea.ts
+++ backend/src/feature/Auth/schemas/AuthSchea.ts
@@ -3,23 +3,17 @@ import { z } from "zod";
 
 export const UserSchema = z.object({
   user_id: z.string().uuid().optional(),
-  name: z.string().min(3, "El nombre de usuario debe tener al menos 3 caracteres"),
-  lastname: z.string().min(3, "El nombre de usuario debe tener al menos 3 caracteres"),
-  username: z
-    .string()
-    .min(3, "El nombre de usuario debe tener al menos 3 caracteres"),
+  name: z.string().min(3, "El nombre debe tener al menos 3 caracteres"),
+  lastname: z.string().min(3, "El apellido debe tener al menos 3 caracteres"),
+  username: z.string().min(3, "El nombre de usuario debe tener al menos 3 caracteres"),
   email: z.string().email("Email inválido"),
-
-  tecnologies: z.array(z.string().min(1, "Cada habilidad debe ser un string no vacío")),
-  profession: z
-    .string()
-    .min(2, "La profesión debe tener al menos 2 caracteres"),
-
+  tecnologies: z.array(z.string().min(1, "Cada tecnología debe ser un string no vacío")),
+  profession: z.string().min(2, "La profesión debe tener al menos 2 caracteres"),
   password: z.string().min(6, "La contraseña debe tener al menos 6 caracteres"),
-
-  portafolio_url: z.string().min(3, "El nombre de usuario debe tener al menos 3 caracteres").optional(),
-  avatar_url: z.string().min(3, "El nombre de usuario debe tener al menos 3 caracteres").optional(),
-  bio: z.string().min(3, "El nombre de usuario debe tener al menos 3 caracteres").optional()
+  created_at: z.date().optional(),
+  portafolio_url: z.string().url("URL de portafolio inválida").optional(),
+  avatar_url: z.string().url("URL de avatar inválida").optional(),
+  bio: z.string().min(3, "La biografía debe tener al menos 3 caracteres").optional()
 });
 
 
